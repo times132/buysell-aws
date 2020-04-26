0b4b3e3c150f45264a5e59bcc3c03e6ea6386321
times132 <times132@gmail.com>
2020-04-26 14:51:51 +0900

    5

--- src/test/java/com/example/giveandtake/repository/UserRepositoryTest.java
+++ src/test/java/com/example/giveandtake/repository/UserRepositoryTest.java
@@ -24,22 +24,22 @@ class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    public void createUser(){
-        userRepository.save(User.builder()
-                .username("yooseunga")
-                .name("sunny")
-                .password("1234")
-                .phone("010-1234-5678")
-                .email("y4380@naver.com")
-                .build());
-
-        List<User> usersList = userRepository.findAll();
-
-        User users = usersList.get(0);
-        assertThat(users.getName(), is("times132"));
-        assertThat(users.getEmail(), is("times132@naver.com"));
-    }
+//    @Test
+//    public void createUser(){
+//        userRepository.save(User.builder()
+//                .username("yooseunga")
+//                .name("sunny")
+//                .password("1234")
+//                .phone("010-1234-5678")
+//                .email("y4380@naver.com")
+//                .build());
+//
+//        List<User> usersList = userRepository.findAll();
+//
+//        User users = usersList.get(0);
+//        assertThat(users.getName(), is("times132"));
+//        assertThat(users.getEmail(), is("times132@naver.com"));
+//    }
 
 //    @Test
 //    public void deleteUser(){
@@ -53,29 +53,29 @@ class UserRepositoryTest {
 //
 //        assertFalse(deleteUser.isPresent()); // false
 //    }
-
-    @Test
-    public void readUser(){
-        Optional<User> user = userRepository.findById(10L);
-
-
-        user.ifPresent(selectUser ->{
-            System.out.println("user: "+selectUser);
-        });
-    }
-//id는 모델에서 long으로 선언 되어 있으므로 L을 붙여 id가 1번인 데이터를 찾는다.
-//데이터를 찾아 user객체에 담아주고 출력한다.
-
-
-    @Test
-    public void AuditTest(){
-        LocalDateTime now = LocalDateTime.now();
-        userRepository.save(User.builder()
-                .username("Park")
-                .name("times132")
-                .password("123456")
-                .phone("010-1234-5678")
-                .email("times132@naver.com")
-                .build());
-    }
+//
+//    @Test
+//    public void readUser(){
+//        Optional<User> user = userRepository.findById(10L);
+//
+//
+//        user.ifPresent(selectUser ->{
+//            System.out.println("user: "+selectUser);
+//        });
+//    }
+////id는 모델에서 long으로 선언 되어 있으므로 L을 붙여 id가 1번인 데이터를 찾는다.
+////데이터를 찾아 user객체에 담아주고 출력한다.
+//
+//
+//    @Test
+//    public void AuditTest(){
+//        LocalDateTime now = LocalDateTime.now();
+//        userRepository.save(User.builder()
+//                .username("Park")
+//                .name("times132")
+//                .password("123456")
+//                .phone("010-1234-5678")
+//                .email("times132@naver.com")
+//                .build());
+//    }
 }
