d655fcb39ab81e3e91d6e3c2c1ece05d14560f25
times132 <times132@gmail.com>
2020-04-26 15:57:15 +0900

    12

--- src/main/java/com/example/giveandtake/GiveandtakeApplication.java
+++ src/main/java/com/example/giveandtake/GiveandtakeApplication.java
@@ -3,6 +3,8 @@ package com.example.giveandtake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -10,7 +12,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EntityScan
 @EnableJpaAuditing
 @EnableCaching
-public class GiveandtakeApplication {
+public class GiveandtakeApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(GiveandtakeApplication.class);
+	}
 
 	public static void main(String[] args) {
 
